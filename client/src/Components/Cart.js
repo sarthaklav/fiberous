d887313e5f3d82f6efaae2a5a518d237d94fe46e
Sarthak Lav <sarthakchaudhary52@gmail.com>
2021-07-04 23:18:34 +0530

    added: cart functionality
    add, increment, decrement and remove functionality is added.

--- client/src/Components/Cart.js
+++ client/src/Components/Cart.js
@@ -1,7 +1,7 @@
 import { useCart } from "../Contexts/cart-context.js";
 
 export function Cart() {
-  const { cart } = useCart();
+  const { cart, dispatch } = useCart();
   return (
     <>
       <div className="cart">
@@ -28,16 +28,38 @@ export function Cart() {
               </div>
               <div className="quantity cart-pill">
                 <div>
-                  <button>-</button>
-                  <span>QTY</span>
-                  <button>+</button>
+                  <button
+                    disabled={item.quantity <= 1}
+                    onClick={() =>
+                      dispatch({ type: "DECREMENT_QUANTITY", payload: item.id })
+                    }
+                  >
+                    -
+                  </button>
+                  <span>{item.quantity}</span>
+                  <button
+                    onClick={() =>
+                      dispatch({ type: "INCREMENT_QUANTITY", payload: item.id })
+                    }
+                  >
+                    +
+                  </button>
                 </div>
               </div>
               <div className="subtotal cart-pill">
-                <div> Sub total </div>
+                <div> Rs {item.price * item.quantity} </div>
               </div>
               <div className="savings cart-pill">
                 <div>savings</div>
+              </div>
+              <div className="remove cart-pill">
+                <span
+                  onClick={() =>
+                    dispatch({ type: "REMOVE_FROM_CART", payload: item.id })
+                  }
+                >
+                  x
+                </span>
               </div>
             </div>
           );
--- client/src/Contexts/cart-context.js
+++ client/src/Contexts/cart-context.js
@@ -1,5 +1,4 @@
 import { createContext, useContext, useReducer } from "react";
-// import { cardImage } from "./card-image.jpeg";
 const CartContext = createContext();
 
 const cart = [
@@ -7,6 +6,7 @@ const cart = [
     id: 1,
     name: "cart wala item",
     image: "http://krisaru.com/wp-content/uploads/2011/06/vege.jpg",
+    quantity: 1,
     price: 25,
   },
 ];
@@ -14,7 +14,32 @@ const cart = [
 function reducerFunc(state, action) {
   switch (action.type) {
     case "ADD_TO_CART":
-      return { ...state, cart: [...state.cart, action.payload] };
+      const itemToAddInCart = { ...action.payload, quantity: 1 };
+      return { ...state, cart: [...state.cart, itemToAddInCart] };
+    case "INCREMENT_QUANTITY":
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+      };
+    case "DECREMENT_QUANTITY":
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        ),
+      };
+    case "REMOVE_FROM_CART":
+      console.log(`line no 38 and remove from cart case is running`);
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload),
+      };
     default:
       return state;
   }
