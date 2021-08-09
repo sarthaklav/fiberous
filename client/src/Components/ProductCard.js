97c1f0f93c519004e8f24672c16d33a5617b9066
Sarthak Lav <sarthakchaudhary52@gmail.com>
2021-08-09 16:37:33 +0530

    refact : refactured product listing page
    added product card component

---
+++ client/src/Components/ProductCard.js
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { useCart } from "../Contexts/cart-context";
+export function ProductCard({ product }) {
+  const { dispatch: cartDispatch } = useCart();
+  return (
+    <div
+      key={product.id}
+      className="card"
+      style={{ margin: "1rem", flex: "200px", minWidth: "100px" }}
+    >
+      <img src={`${product.image}`} alt="product" />
+
+      <div className="card-details">
+        <NavLink to={`/products/${product.id}`}>
+          <h4 className="side-nav-heading">{product.name}</h4>
+        </NavLink>
+        <span>
+          Rs.{product.price} for {product.packet}{" "}
+        </span>
+        {!product.inStock && <p style={{ color: "red" }}>Out of stock </p>}
+        {product.fastDelivery && <p>Fast Delivery available</p>}
+        <p style={{ color: "red" }}>{product.offer}</p>
+      </div>
+      <button
+        onClick={() => cartDispatch({ type: "ADD_TO_CART", payload: product })}
+        className="btn btn-secondary"
+      >
+        Add to cart
+      </button>
+    </div>
+  );
+}
--- client/src/Components/Products.js
+++ client/src/Components/Products.js
@@ -1,11 +1,9 @@
-import { useReducer } from "react";
-import { NavLink } from "react-router-dom";
-import { useCart } from "../Contexts/cart-context";
-import products from "../data";
 import { Sidebar } from "./Sidebar";
+import { ProductCard } from "./ProductCard";
+import products from "../data";
 import { viewReducer, getSortedData, getFilteredProducts } from "../utilities";
+import { useReducer } from "react";
 export function Products() {
-  const { dispatch: cartDispatch } = useCart();
   const [{ sortBy, showAllInventory, showFastDelivery }, viewDispatch] =
     useReducer(viewReducer, {
       sortBy: null,
@@ -19,44 +17,16 @@ export function Products() {
   });
   return (
     <>
-      <Sidebar
-        viewDispatch={viewDispatch}
-        sortBy={sortBy}
-        showAllInventory={showAllInventory}
-        showFastDelivery={showFastDelivery}
-      />
       <div className="products-page">
+        <Sidebar
+          viewDispatch={viewDispatch}
+          sortBy={sortBy}
+          showAllInventory={showAllInventory}
+          showFastDelivery={showFastDelivery}
+        />
         <div className="container-products">
           {filteredProducts.map((product) => (
-            <div
-              key={product.id}
-              className="card"
-              style={{ margin: "1rem", flex: "200px", minWidth: "100px" }}
-            >
-              <img src={`${product.image}`} alt="product" />
-
-              <div className="card-details">
-                <NavLink to={`/products/${product.id}`}>
-                  <h4 className="side-nav-heading">{product.name}</h4>
-                </NavLink>
-                <span>
-                  Rs.{product.price} for {product.packet}{" "}
-                </span>
-                {!product.inStock && (
-                  <p style={{ color: "red" }}>Out of stock </p>
-                )}
-                {product.fastDelivery && <p>Fast Delivery available</p>}
-                <p style={{ color: "red" }}>{product.offer}</p>
-              </div>
-              <button
-                onClick={() =>
-                  cartDispatch({ type: "ADD_TO_CART", payload: product })
-                }
-                className="btn btn-secondary"
-              >
-                Add to cart
-              </button>
-            </div>
+            <ProductCard product={product} />
           ))}
         </div>
       </div>
